6c73b977d5cb9a19b429ba3da437c45668fedc50
Niway <cniway94@gmail.com>
2025-08-05 23:26:04 +0300

    update the logout function

--- src/components/layout/TitleBar.jsx
+++ src/components/layout/TitleBar.jsx
@@ -1,3 +1,41 @@
+// import React, { useContext } from "react";
+// import Button from "../ui/Button";
+// import { LogOut } from "lucide-react";
+// import { Menu, X } from "lucide-react";
+// import { SideBarContext } from "../../contexts/SideBarProvider";
+
+// import { useNavigate } from "react-router-dom";
+
+// const TitleBar = ({ title }) => {
+//   const SideBarCtx = useContext(SideBarContext);
+//   const navigate = useNavigate();
+//   const handleNavigate = () => {navigate("/")};
+
+//   return (
+//     <div className="flex bg-titleBarColor py-2 px-6 rounded-lg justify-between items-center mb-4">
+//       <button
+//         onClick={SideBarCtx.toggleSidebar}
+//         className="text-gray-600 focus:outline-none"
+//       >
+//         {SideBarCtx.isSidebarOpen ? (
+//           <X className="h-6 w-6" />
+//         ) : (
+//           <Menu className="h-6 w-6" />
+//         )}
+//       </button>
+
+//       <h3 className="font-serif text-3xl font-bold">{title}</h3>
+
+//       <Button variant="ghost" className="flex items-center gap-1 " onClick={handleNavigate}>
+
+//         <LogOut size={25} /> Log Out
+//       </Button>
+//     </div>
+//   );
+// };
+
+// export default TitleBar;
+
 import React, { useContext } from "react";
 import Button from "../ui/Button";
 import { LogOut } from "lucide-react";
@@ -9,7 +47,22 @@ import { useNavigate } from "react-router-dom";
 const TitleBar = ({ title }) => {
   const SideBarCtx = useContext(SideBarContext);
   const navigate = useNavigate();
-  const handleNavigate = () => {navigate("/")};
+  const handleLogout = async () => {
+    try {
+      const res = await fetch(`${process.env.REACT_APP_DEPLOYMENT_LINK}/admin/logout`, {
+        method: "POST",
+        credentials: "include", // IMPORTANT to send cookies
+      });
+
+      if (res.ok) {
+        navigate("/"); // Redirect to login/home page after successful logout
+      } else {
+        console.error("Logout failed.");
+      }
+    } catch (error) {
+      console.error("An error occurred during logout:", error);
+    }
+  };
 
   return (
     <div className="flex bg-titleBarColor py-2 px-6 rounded-lg justify-between items-center mb-4">
@@ -26,11 +79,11 @@ const TitleBar = ({ title }) => {
 
       <h3 className="font-serif text-3xl font-bold">{title}</h3>
 
-
-      <Button variant="ghost" className="flex items-center gap-1 " onClick={handleNavigate}>
-      
-        
-
+      <Button
+        variant="ghost"
+        className="flex items-center gap-1 "
+        onClick={handleLogout}
+      >
         <LogOut size={25} /> Log Out
       </Button>
     </div>
